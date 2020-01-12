b24df59091575efdf22ff40080ffd174ce379e4c
Dario Nascimento <dario.nascimento@cba.com.au>
2020-01-12 21:00:48 +1100

    deploy

--- lib/cdk-pipeline-stack.ts
+++ lib/cdk-pipeline-stack.ts
@@ -5,7 +5,7 @@ export class CdkPipelineStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const bucket = new s3.Bucket(this, 'MyBucket', {
+    const bucket = new s3.Bucket(this, 'bucket', {
       bucketName: "dario-bucket",
       removalPolicy: cdk.RemovalPolicy.DESTROY
     });
