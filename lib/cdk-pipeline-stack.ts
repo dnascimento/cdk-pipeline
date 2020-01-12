47ab3f63d415b4944f7e96ae0889b5f2a617c3b5
Dario Nascimento <dario.nascimento@cba.com.au>
2020-01-12 20:35:33 +1100

    cdk

--- lib/cdk-pipeline-stack.ts
+++ lib/cdk-pipeline-stack.ts
@@ -1,9 +1,13 @@
 import * as cdk from '@aws-cdk/core';
+import s3 = require('@aws-cdk/aws-s3');
 
 export class CdkPipelineStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const bucket = new s3.Bucket(this, 'MyBucket', {
+      bucketName: "dario-bucket",
+      removalPolicy: cdk.RemovalPolicy.DESTROY
+    });
   }
 }
